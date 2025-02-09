c515f0fbe4286a923277d727a9ad8e7a02b50f91
Sayyad sheksha <syedshaikshavali@gmail.com>
2025-02-09 15:15:14 +0530

    stack reverse method  commit

--- Stack/Stackcm.java
+++ Stack/Stackcm.java
@@ -4,18 +4,28 @@ public class Stackcm {
      public static void  PushAtButtom(int data,Stack<Integer>s){
         if(s.isEmpty()){
             s.push(data);
+            return ;
         }
         int top=s.pop();
         PushAtButtom(data, s);
         s.push(top);
+     }
 
+     public static  void revese(Stack<Integer> s){
+        if(s.isEmpty()){
+            return ;
+        }
+        int top=s.pop();
+        revese(s);
+        PushAtButtom(top, s);
+       
      }
     public static void main(String[] args) {
-         Stack <Integer> s=new Stack();
+         Stack <Integer> s=new Stack<>();
         s.push(3);
         s.push(8);
         s.push(88);
-   PushAtButtom(6, s);
+   revese(s);
         System.out.println(s);
     }
 }
