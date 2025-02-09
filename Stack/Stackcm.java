9eab205706347cff160eccfdf953891b49d1b9c9
Sayyad sheksha <syedshaikshavali@gmail.com>
2025-02-09 14:49:52 +0530

    Stack push at buttom

--- Stack/Stackcm.java
+++ Stack/Stackcm.java
@@ -1,11 +1,21 @@
-import  java.util.*;
+import  java.util.Stack;
 public class Stackcm {
     
+     public static void  PushAtButtom(int data,Stack<Integer>s){
+        if(s.isEmpty()){
+            s.push(data);
+        }
+        int top=s.pop();
+        PushAtButtom(data, s);
+        s.push(top);
+
+     }
     public static void main(String[] args) {
          Stack <Integer> s=new Stack();
         s.push(3);
         s.push(8);
         s.push(88);
+   PushAtButtom(6, s);
         System.out.println(s);
     }
 }
